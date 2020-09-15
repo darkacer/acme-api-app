0f2132b93e13b3ec1562407e11b5562bf7e82406
darkacer <omkardeokar95@gmail.com>
2020-09-16 00:40:02 +0530

    update from postman

--- index.js
+++ index.js
@@ -23,6 +23,7 @@ app.get('/oauthcallback', (req, res) => {
     let code = req.query.code
     console.log('code is ', code)
     code = decodeURIComponent(code)
+    /*
     const url ='https://login.salesforce.com/services/oauth2/token';
     const headers = {
         "grant_type": "authorization_code",
@@ -33,7 +34,7 @@ app.get('/oauthcallback', (req, res) => {
         "redirect_uri": "https://acme-api-app.herokuapp.com/oauthcallback"
     }
 
-    fetch(url, { method: 'POST', headers: headers})
+    fetch(url, { method: 'POST', body: headers})
     .then((res) => {
         return res.json()
     })
@@ -42,6 +43,37 @@ app.get('/oauthcallback', (req, res) => {
      console.log(json);
    
    });
+   */
+
+  var myHeaders = new Headers();
+  myHeaders.append("authorization", "TX0cBnc30W1KDKpAzb4ci147ZIaiqO8q8TyQzPDUnW8l8qfbytYZkZNWTpaR");
+  myHeaders.append("sender_id", "FSTSMS");
+  myHeaders.append("message", "This is test message");
+  myHeaders.append("language", "english");
+  myHeaders.append("route", "p");
+  myHeaders.append("numbers", "9921181839");
+  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
+  myHeaders.append("Cookie", "BrowserId=ru_7xOZAEeq8lH964gzipA");
+  
+  var urlencoded = new URLSearchParams();
+  urlencoded.append("grant_type", "authorization_code");
+  urlencoded.append("client_id", "3MVG9d8..z.hDcPIA4C3lokx_ddUobFVY2b_LLeemoWaWaTizjDd0pj_7WCz5KLVmXlZ25F8tz5WV2kjcsszB");
+  urlencoded.append("code", code);
+  urlencoded.append("client_secret", "E210A8C1A61FAB465DF79642B7A3149D356C40A91448ED5D3582BA61A8E7E8EB");
+  urlencoded.append("redirect_uri", "https://acme-api-app.herokuapp.com/oauthcallback");
+  urlencoded.append("Content-type", "application/x-www-form-urlencoded");
+
+  var requestOptions = {
+    method: 'POST',
+    headers: myHeaders,
+    body: urlencoded,
+    redirect: 'follow'
+  };
+  
+  fetch("https://login.salesforce.com/services/oauth2/token", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log('result', result))
+    .catch(error => console.log('error', error));
 
 })
 
