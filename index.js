9bfb9a24539a45eef92ad092d3762e13cc5c3ef6
darkacer <omkardeokar95@gmail.com>
2020-09-16 00:48:49 +0530

    using axios

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express")
 const app = express()
-const fetch = require('node-fetch');
+var axios = require('axios');
+var qs = require('qs');
 const path = require('path');
 let port = process.env.PORT || 3000;
 
@@ -45,35 +46,39 @@ app.get('/oauthcallback', (req, res) => {
    });
    */
 
-  var myHeaders = new Headers();
-  myHeaders.append("authorization", "TX0cBnc30W1KDKpAzb4ci147ZIaiqO8q8TyQzPDUnW8l8qfbytYZkZNWTpaR");
-  myHeaders.append("sender_id", "FSTSMS");
-  myHeaders.append("message", "This is test message");
-  myHeaders.append("language", "english");
-  myHeaders.append("route", "p");
-  myHeaders.append("numbers", "9921181839");
-  myHeaders.append("Content-Type", "application/x-www-form-urlencoded");
-  myHeaders.append("Cookie", "BrowserId=ru_7xOZAEeq8lH964gzipA");
-  
-  var urlencoded = new URLSearchParams();
-  urlencoded.append("grant_type", "authorization_code");
-  urlencoded.append("client_id", "3MVG9d8..z.hDcPIA4C3lokx_ddUobFVY2b_LLeemoWaWaTizjDd0pj_7WCz5KLVmXlZ25F8tz5WV2kjcsszB");
-  urlencoded.append("code", code);
-  urlencoded.append("client_secret", "E210A8C1A61FAB465DF79642B7A3149D356C40A91448ED5D3582BA61A8E7E8EB");
-  urlencoded.append("redirect_uri", "https://acme-api-app.herokuapp.com/oauthcallback");
-  urlencoded.append("Content-type", "application/x-www-form-urlencoded");
 
-  var requestOptions = {
-    method: 'POST',
-    headers: myHeaders,
-    body: urlencoded,
-    redirect: 'follow'
-  };
-  
-  fetch("https://login.salesforce.com/services/oauth2/token", requestOptions)
-    .then(response => response.text())
-    .then(result => console.log('result', result))
-    .catch(error => console.log('error', error));
+    var data = qs.stringify({
+        'grant_type': 'authorization_code',
+        'client_id': '3MVG9d8..z.hDcPIA4C3lokx_ddUobFVY2b_LLeemoWaWaTizjDd0pj_7WCz5KLVmXlZ25F8tz5WV2kjcsszB',
+        'code': code,
+        'client_secret': 'E210A8C1A61FAB465DF79642B7A3149D356C40A91448ED5D3582BA61A8E7E8EB',
+        'redirect_uri': 'https://acme-api-app.herokuapp.com/oauthcallback',
+        'Content-type': 'application/x-www-form-urlencoded' 
+    });
+    var config = {
+        method: 'post',
+        url: 'https://login.salesforce.com/services/oauth2/token',
+        headers: { 
+            'authorization': 'TX0cBnc30W1KDKpAzb4ci147ZIaiqO8q8TyQzPDUnW8l8qfbytYZkZNWTpaR', 
+            'sender_id': 'FSTSMS', 
+            'message': 'This is test message', 
+            'language': 'english', 
+            'route': 'p', 
+            'numbers': '9921181839', 
+            'Content-Type': 'application/x-www-form-urlencoded', 
+            'Cookie': 'BrowserId=ru_7xOZAEeq8lH964gzipA'
+        },
+        data : data
+    };
+
+    axios(config)
+    .then(function (response) {
+        console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+        console.log(error);
+    });
+
 
 })
 
