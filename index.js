4e5e83f7898324d7b645232d09524f2e61f84a78
darkacer <omkardeokar95@gmail.com>
2020-09-17 13:46:31 +0530

    using env vars

--- index.js
+++ index.js
@@ -6,13 +6,15 @@ const path = require('path');
 let port = process.env.PORT || 3000;
 
 let dataInput = 'im before data'
+let cartData = [];
+let showCart = false;
 
 app.get('/', function(req, res) {
     res.send('hello world')
 }) 
 
 app.get("/login", function (req, res) {
-    res.sendFile(path.join(__dirname+'/index.html'));
+    res.sendFile(path.join(__dirname+'/public/login.html'));
 });
 
 app.post('/setData', (req, res) => {
@@ -24,34 +26,12 @@ app.get('/oauthcallback', (req, res) => {
     let code = req.query.code
     console.log('code is ', code)
     code = decodeURIComponent(code)
-    /*
-    const url ='https://login.salesforce.com/services/oauth2/token';
-    const headers = {
-        "grant_type": "authorization_code",
-        "Content-Type": "application/x-www-form-urlencoded",
-        "client_id": "3MVG9d8..z.hDcPIA4C3lokx_ddUobFVY2b_LLeemoWaWaTizjDd0pj_7WCz5KLVmXlZ25F8tz5WV2kjcsszB",
-        "client_secret": "E210A8C1A61FAB465DF79642B7A3149D356C40A91448ED5D3582BA61A8E7E8EB",
-        "code": code,
-        "redirect_uri": "https://acme-api-app.herokuapp.com/oauthcallback"
-    }
-
-    fetch(url, { method: 'POST', body: headers})
-    .then((res) => {
-        return res.json()
-    })
-   .then((json) => {
-      // Do something with the returned data.
-     console.log(json);
-   
-   });
-   */
-
 
     var data = qs.stringify({
         'grant_type': 'authorization_code',
-        'client_id': '3MVG9d8..z.hDcPIA4C3lokx_ddUobFVY2b_LLeemoWaWaTizjDd0pj_7WCz5KLVmXlZ25F8tz5WV2kjcsszB',
+        'client_id': process.env.client_id,
         'code': code,
-        'client_secret': 'E210A8C1A61FAB465DF79642B7A3149D356C40A91448ED5D3582BA61A8E7E8EB',
+        'client_secret': process.env.client_secret,
         'redirect_uri': 'https://acme-api-app.herokuapp.com/oauthcallback',
         'Content-type': 'application/x-www-form-urlencoded' 
     });
@@ -64,7 +44,7 @@ app.get('/oauthcallback', (req, res) => {
             'message': 'This is test message', 
             'language': 'english', 
             'route': 'p', 
-            'numbers': '9921181839', 
+            'numbers': '', 
             'Content-Type': 'application/x-www-form-urlencoded', 
             'Cookie': 'BrowserId=ru_7xOZAEeq8lH964gzipA'
         },
@@ -84,12 +64,21 @@ app.get('/oauthcallback', (req, res) => {
 })
 
 
-app.post("/cartdetails", (req, res) => {
-    let cartdata = req.query.cart;
-    console.log('cart detials are ');
+app.post("/setCartDetails", (req, res) => {
+    let cartdata = JSON.parse(req.query.cart)
+    console.log('cart detials are ' + cartdata);
     let respo = ''
     cartdata.forEach(el => respo += JSON.stringify(el))
     res.send(respo)
+})
+
+app.get("/getCartDetails", () => {
+    
+})
+
+app.post("/resetCart", (req, res) => {
+    showCart = false;
+    cartData = [];
 })
 
 app.get("/getData", function (req, res) {
